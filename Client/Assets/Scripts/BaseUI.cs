8cb4d29d6fab082139ad8058860679604b81fb70
davidsheh <davidsheh1015@gmail.com>
2019-06-05 17:49:47 +0800

    add Client project

---
+++ Client/Assets/Scripts/BaseUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseUI : MonoBehaviour
+{
+    public Image img;
+    public Button btn;
+    public Text text;
+}
---
+++ Client/Assets/Scripts/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    ILRuntime.Runtime.Enviorment.AppDomain appdomain;
+    void Start()
+    {
+        StartCoroutine(LoadILRuntime());
+    }
+
+    IEnumerator LoadILRuntime()
+    {
+        appdomain = new ILRuntime.Runtime.Enviorment.AppDomain();
+#if UNITY_ANDROID
+    WWW www = new WWW(Application.streamingAssetsPath + "/Hotfix.dll");
+#else
+        WWW www = new WWW("file:///" + Application.streamingAssetsPath + "/Hotfix.dll");
+#endif
+        while (!www.isDone)
+            yield return null;
+        if (!string.IsNullOrEmpty(www.error))
+            Debug.LogError(www.error);
+        byte[] dll = www.bytes;
+        www.Dispose();
+#if UNITY_ANDROID
+    www = new WWW(Application.streamingAssetsPath + "/Hotfix.pdb");
+#else
+        www = new WWW("file:///" + Application.streamingAssetsPath + "/Hotfix.pdb");
+#endif
+        while (!www.isDone)
+            yield return null;
+        if (!string.IsNullOrEmpty(www.error))
+            Debug.LogError(www.error);
+        byte[] pdb = www.bytes;
+        using (System.IO.MemoryStream fs = new MemoryStream(dll))
+        {
+            using (System.IO.MemoryStream p = new MemoryStream(pdb))
+            {
+                appdomain.LoadAssembly(fs, p, new Mono.Cecil.Pdb.PdbReaderProvider());
+            }
+        }
+        OnILRuntimeInitialized();
+    }
+
+    void OnILRuntimeInitialized()
+    {
+#if UNITY_EDITOR
+        appdomain.DebugService.StartDebugService(56000);
+#endif
+
+        appdomain.DelegateManager.RegisterDelegateConvertor<UnityEngine.Events.UnityAction>((act) =>
+        {
+            return new UnityEngine.Events.UnityAction(() =>
+            {
+                ((Action)act)();
+            });
+        });        
+        appdomain.Invoke("Hotfix.Game", "Initialize", null, null);
+    }
+
+    public void OnBtnClick()
+    {
+        appdomain.Invoke("Hotfix.Game", "ClickBtn", null, null);
+    }
+}
