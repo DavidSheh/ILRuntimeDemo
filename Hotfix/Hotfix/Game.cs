e13f5ecf931976893835f3581ca3c1c3042ae832
davidsheh <davidsheh1015@gmail.com>
2019-06-05 17:50:11 +0800

    add Hotfix project

---
+++ Hotfix/Hotfix/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hotfix
+{
+    public class Game
+    {
+        public static void Initialize()
+        {
+            Debug.Log("Initialize ...");
+            // 初始化
+        }
+
+        public static void ClickBtn()
+        {
+            Debug.Log("ClickBtn ...");
+
+            // 显示 UI
+            var prefab = Resources.Load<GameObject>("UITest");
+            var obj = GameObject.Instantiate(prefab);
+            obj.transform.SetParent(GameObject.Find("UIRoot").transform);
+            obj.transform.localPosition = Vector3.zero;
+            var baseUI = obj.GetComponent<BaseUI>();
+            new UILogic(baseUI);
+        }
+    }
+}
---
+++ Hotfix/Hotfix/UILogic.cs
@@ -0,0 +1,14 @@
+﻿namespace Hotfix
+{
+    public class UILogic
+    {
+        public UILogic(BaseUI ui)
+        {
+            ui.btn.onClick.AddListener(() => 
+            {
+                ui.text.text = "Click Button!";
+                ui.img.color = UnityEngine.Color.red;
+            });
+        }
+    }
+}
